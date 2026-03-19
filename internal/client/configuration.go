eaa01bf5e47bfbe082d7a5c1fabcb69009b53b40
Mias van Klei <miasvanklei@outlook.com>
2026-03-19 13:48:37 +0100

    detect duplicate server entries

--- internal/client/configuration.go
+++ internal/client/configuration.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"runtime"
+	"fmt"
 
 	"github.com/BurntSushi/toml"
 )
@@ -27,8 +28,26 @@ func ParseConfiguration(filePath string) (*Configuration, error) {
 		ConnectionTimeout: 15,      // 15 seconds
 		ClientID:          "",
 	}
+
 	if _, err := toml.DecodeFile(filePath, &config); err != nil {
 		return nil, err
 	}
+
+	if err := detectDuplicateServers(config.Servers); err != nil {
+		return nil, err
+        }
+
 	return &config, nil
+}
+
+func detectDuplicateServers(servers []string) error {
+	mapDuplicate := make(map[string]bool)
+
+	for _, server := range servers {
+		if mapDuplicate[server] {
+			return fmt.Errorf("Duplicate entry found: %s", server)
+		}
+	}
+
+	return nil
 }
